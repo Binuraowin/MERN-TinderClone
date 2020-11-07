7d7614fbed01e22d779e336882551240efbb1a74
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2020-11-08 01:41:14 +0530

    added basic endpoints

--- tinder-backend/server.js
+++ tinder-backend/server.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import Cors from 'cors';
+import cards from './dpCard.js'
 
 
 
 const app = express();
 const port = process.env.PORT || 8001;
 const uri = process.env.ATLAS_URI;
+
+app.use(express.json())
+app.use(Cors());
 
 mongoose.connect(connection_url, {
     useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true 
@@ -19,5 +24,30 @@ connection.once('open',() =>{
 app.get('/',(req, res) =>{
     res.status(200).send("Hellooo");
 });
+app.post('/tinder/card',(req,res) =>{
+    const dbCard = req.body;
+
+    cards.create(dbCard,(err,data) =>{
+        if(err){
+            res.status(500).send(err)
+        }else {
+            res.status(201).send(data)
+        }
+    })
+});
+
+app.get('/tinder/card',(req,res) =>{
+
+
+    cards.find((err,data) =>{
+        if(err){
+            res.status(500).send(err)
+        }else {
+            res.status(200).send(data)
+        }
+    })
+});
+
+ 
 
 app.listen(port,() => console.log(`listening ${port}`));
