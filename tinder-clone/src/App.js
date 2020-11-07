d4548c9806105424762ed6d709b7c00650f57c60
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2020-11-07 16:07:02 +0530

    tinder card

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
-import Header from "./Header"
+import Header from "./Header";
+import TinderCard from "./TinderCards";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <TinderCard/>
     </div>
   );
 }
---
+++ tinder-clone/src/TinderCards.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import "./TinderCards.css";
+import TinderCard from "react-tinder-card";
+import elon from "./images/190717-Elon-Musk_16bfef37f8b_medium.jpg"
+
+function TinderCards() {
+    const [people, setPeople] = useState([
+        {
+            name:"Elon Musk",
+            url:
+            "https://cdn.vox-cdn.com/thumbor/FGxE-lBozP6CLLZgDKdtOGActxk=/0x0:2040x1360/1220x813/filters:focal(857x517:1183x843):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/67108186/elon_musk_tesla_3036.0.jpg"
+        },
+        {
+            name:"Jeff Bezos",
+            url:
+            "https://content.fortune.com/wp-content/uploads/2020/02/GettyImages-1078542150.jpg?resize=1500,1000"
+        },
+        {
+            name:"Binura owin",
+            url:
+            "https://avatars2.githubusercontent.com/u/53115861?s=460&u=2c9cf0e6974a97f7ca50ea9fb406f9a65f6c13c5&v=4"
+        },
+    ]);
+
+    const swiped = (direction, nameToDelete) =>{
+        console.log("removing" + nameToDelete);
+        // setLastDirection(direction);
+    };
+    const outOfFrame = (name) =>{
+        console.log(name + "left the screen");
+    }
+
+
+    return (
+        <div className="tinderCards">
+            <div className="tinderCards__cardContainer">
+            {people.map((person) => (
+                <TinderCard className="swipe"
+                key={person.name}
+                preventSwipe={["up","down"]}
+                onSwipe={(dir) => swiped(dir, person.name)}
+                onCardLeftScreen ={() => outOfFrame(person.name)}
+                >
+                    <div style={{backgroundImage:`url('${person.url}')`}}
+                    className="card"
+                    >
+                    <h3>{person.name}</h3>
+                    </div>
+
+                </TinderCard>
+                
+            ))}
+            </div>      
+        </div>
+    );
+}
+
+export default TinderCards
