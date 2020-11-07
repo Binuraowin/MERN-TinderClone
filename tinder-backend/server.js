561bc6cf1e5e3861ca793be4842ba037d13da73b
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2020-11-07 23:52:24 +0530

    Update server.js

--- tinder-backend/server.js
+++ tinder-backend/server.js
@@ -1,3 +1,23 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
+
+
+const app = express();
+const port = process.env.PORT || 8001;
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(connection_url, {
+    useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true 
+})
+const connection = mongoose.connection;
+connection.once('open',() =>{
+    console.log("MongoDB database connection established succcessfully");
+})
+
+
+app.get('/',(req, res) =>{
+    res.status(200).send("Hellooo");
+});
+
+app.listen(port,() => console.log(`listening ${port}`));
